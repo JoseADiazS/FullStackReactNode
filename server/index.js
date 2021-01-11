074b044d39c0f4458a7e997650f8876d401e8691
Jose Diaz <jdiazseg9611@universidadean.edu.co>
2021-01-11 11:33:30 -0500

    commit

--- server/index.js
+++ server/index.js
@@ -7,3 +7,6 @@ app.get('/', (req,res) =>{
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// https://nameless-bayou-20772.herokuapp.com/ 
+// https://git.heroku.com/nameless-bayou-20772.gitp
